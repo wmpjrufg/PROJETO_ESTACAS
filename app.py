efd74d0c56f1ccd42e4735b9f1ad338939eb6e28
nerodrakar <luizhenriquerezio@gmail.com>
2024-03-25 15:32:24 -0300

    feat: atualização das funções carregar_dados() e capacidade_carga()

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 import streamlit as st 
 from funcoes import *
 
+html_text = """
+<h1 style='color: blue;'>Projeto Estacas</h1>
+<h2 style='color: green;'>Cabeçalho</h2>
+<p align="justify">
+<p>Lorem ipsum dolor sit amet. Qui voluptas odio ea blanditiis quae non numquam internos sed Quis repudiandae qui expedita sint.
+Quo animi consectetur ea officia voluptate et delectus totam cum amet veniam! Non quod incidunt sed rerum vitae ea soluta libero.
+Sit delectus quibusdam et nulla galisum sed esse assumenda.</p>
+</a>
+"""
+
+# Renderizar o texto HTML
+st.markdown(html_text, unsafe_allow_html=True)
+
 df, nome = carregar_dados()
 
 if df is not None:
-    diametro, tipo = estaca_info()
-    novo_df = capacidade_carga(df, diametro, tipo)
+    diametro, f_ck, tipo = estaca_info()
+    novo_df = capacidade_carga(df, diametro, f_ck, tipo)
     download_excel(novo_df, nome)
--- funcoes.py
+++ funcoes.py
@@ -4,8 +4,6 @@ import tempfile
 from estacas import *
 
 def carregar_dados():
-    st.title("Projeto Estacas")
-
     uploaded_file = st.file_uploader("Selecione um arquivo Excel", type=["xlsx", "xls"])
 
     if uploaded_file is not None:
@@ -14,7 +12,16 @@ def carregar_dados():
             st.subheader("Base de dados inicial:")
             st.dataframe(df)
             nome_df = uploaded_file.name
-            return df, nome_df # Retornando o DataFrame para que possa ser utilizado em outras partes do código
+            
+            # Verifique se o DataFrame tem todas as colunas necessárias
+            colunas_necessarias = ['profundidade (m)', 'NSPT', 'diâmetro (m)', 'Solo', 'Solo Aoki-Veloso', 'Solo Decourt-Quaresma']
+            # Verifica se todas as colunas necessárias estão presentes no DataFrame
+            colunas_ausentes = [coluna for coluna in colunas_necessarias if coluna not in df.columns]
+            
+            if colunas_ausentes:
+                raise ValueError(f"colunas ausentes: {', '.join(colunas_ausentes)}")
+            
+            return df, nome_df # Retornando o DataFrame 
         except Exception as e:
             st.error(f"Ocorreu um erro ao carregar o arquivo: {str(e)}")
             return None, None  # Retornando None em caso de erro
@@ -22,46 +29,67 @@ def carregar_dados():
     else:
         st.warning("Por favor, faça o upload de um arquivo Excel.")
         return None, None
+    
         
 def estaca_info():
     st.title("Informações da Estaca")
     
     diametro_estaca = st.number_input("Digite o diâmetro da estaca em metros:", min_value=0.0)
+    f_ck = st.number_input("Digite a tensão de compressão característica do concreto (kPa):", min_value=0.0)
     tipo_estaca = st.selectbox("Selecione o tipo da estaca:", ["Franki", "Metálica", "Pré-moldada de concreto", "Escavada"])
-
     if st.button('Adicionar Informações'):
-        return diametro_estaca, tipo_estaca.lower()
+        return diametro_estaca, f_ck, tipo_estaca.lower()
 
     else:
-        return None, None
+        return None, None, None
 
 
-def capacidade_carga(df, diametro_estaca, tipo_estaca):
+def capacidade_carga(df, diametro_estaca, f_ck, tipo_estaca):
     try: 
         if diametro_estaca is not None and tipo_estaca is not None:
             st.title("Capacidade de Carga")
-            df['tipo estaca'] = tipo_estaca
-            df['diâmetro (m)'] = diametro_estaca
+            df['tipo estaca'] = tipo_estaca.lower()
             df['area estaca (m2)'] = df.apply(area_estaca, axis=1)
             df['perimetro estaca (m)'] = df.apply(perimetro_estaca, axis=1)
-            df['k'] = df.apply(k_solo, axis=1, args=(k_alpha,))
-            df['alpha'] = df.apply(alpha_solo, axis=1, args=(k_alpha,))
-            df['f_1'] = df.apply(f1, axis=1, args=(f_1,))
+            df['k'] = df.apply(k_solo, axis=1)
+            df['alpha'] = df.apply(alpha_solo, axis=1)
+            df['f_1'] = df.apply(f1, axis=1)
             df['f_2'] = 2 * df['f_1']
-            df['r_p (kPa)'] = df.apply(tensao_ponta, axis=1)
-            df['P_p (kN)'] = df.apply(carga_ponta, axis=1)
-            df['r_l (kPa)'] = df.apply(tensao_lateral, axis=1)
-            df['P_l (kN)'] = df.apply(carga_lateral, axis=1)
+            df['r_p [Aoki-Veloso] (kPa)'] = df.apply(tensao_ponta_aoki_veloso, axis=1)
+            df['P_p [Aoki-Veloso] (kN)'] = df.apply(carga_ponta_aoki_veloso, axis=1)
+            df['r_l [Aoki-Veloso] (kPa)'] = df.apply(tensao_lateral_aoki_veloso, axis=1)
             soma_resultados = []
-            r_l = df['r_l (kPa)'].to_list()
+            r_l = df['r_l [Aoki-Veloso] (kPa)'].to_list()
             for i in range(len(df)):
                 if i == 0:
-                    soma = df.iloc[i]['r_p (kPa)'] + df.iloc[i]['r_l (kPa)']
+                    soma = df.iloc[i]['r_l [Aoki-Veloso] (kPa)']
                 else:
-                    soma = df.iloc[i]['r_p (kPa)'] + sum(r_l[:i+1])
+                    soma = sum(r_l[:i+1])
                 soma_resultados.append(soma)
-            df['r_rd [Aoki-Veloso] (kPa)'] = soma_resultados
-            df['P_rd [Aoki-Veloso] (kN)'] = df.apply(carga_total, axis=1)
+            df['r_l acumulado [Aoki-Veloso] (kPa)'] = soma_resultados
+            df['P_l [Aoki-Veloso] (kN)'] = df.apply(carga_lateral_aoki_veloso, axis=1)
+            df['r_rd [Aoki-Veloso] (kPa)'] = df['r_l acumulado [Aoki-Veloso] (kPa)'] + df['r_p [Aoki-Veloso] (kPa)']
+            df['P_rd [Aoki-Veloso] (kN)'] = df['P_p [Aoki-Veloso] (kN)'] + df['P_l [Aoki-Veloso] (kN)']
+            df['c'] = df.apply(c_solo, axis=1)
+            df['r_p [Decourt-Quaresma] (kPa)'] = df.apply(tensao_ponta_decorto_quaresma, axis=1)
+            df['P_p [Decourt-Quaresma] (kN)'] = df.apply(carga_ponta_decourt_quaresma, axis=1)
+            n_spt = df['NSPT'].to_list()
+            soma_resultados = []
+            for i in range(len(df)):
+                if i == 0:
+                    soma = df.iloc[i]['NSPT']
+                else:
+                    soma = sum(n_spt[:i+1]) / (i+1)
+                soma_resultados.append(soma)
+            df['nspt_medio'] = soma_resultados
+            df['r_l acumulado [Decourt-Quaresma] (kPa)'] = df.apply(tensao_lateral_decourt_quaresma, axis=1)
+            df['P_l [Decourt-Quaresma] (kN)'] = df.apply(carga_lateral_decourt_quaresma, axis=1)
+            df['r_rd [Decourt-Quaresma] (kPa)'] = df['r_l acumulado [Decourt-Quaresma] (kPa)'] + df['r_p [Decourt-Quaresma] (kPa)']
+            df['P_rd [Decourt-Quaresma] (kN)'] = df['P_p [Decourt-Quaresma] (kN)'] + df['P_l [Decourt-Quaresma] (kN)']
+            
+            df['P_rd (kN)'] = df[['P_rd [Decourt-Quaresma] (kN)', 'P_rd [Aoki-Veloso] (kN)']].min(axis=1)
+            df['f_cd (kPa)'] = f_ck / 3.1
+            df['P_Rd (kN)'] = 0.85 * df['f_cd (kPa)'] * df['area estaca (m2)']
             st.write(df)
             return df
     except Exception as e:
